d7ac531bf820321d9d9374462e0703acb114d0f3
Jason Merino <jason.merino@build.com>
2013-11-13 13:54:06 -0800

    Added stubs for finish collections and models.

---
+++ js/collections/finishes.js
@@ -0,0 +1,15 @@
+define([
+	'underscore',
+	'backbone',
+	'models/finish'
+], function(_, Backbone, FinishModel) {
+
+	var FinishesCollection = Backbone.Collection.extend({
+
+		model: FinishModel
+
+	});
+
+	return FinishesCollection;
+
+});
---
+++ js/models/finish.js
@@ -0,0 +1,16 @@
+define([
+	'underscore',
+	'backbone'
+], function(_, Backbone) {
+
+	var FinishModel = Backbone.Model.extend({
+
+		defaults: {
+
+		}
+
+	});
+
+	return FinishModel;
+
+});
